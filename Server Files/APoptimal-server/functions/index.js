0421cb0c9e46824f4477aef7839b9419f9cd9a0a
Yanai99 <yanaiparan@gmail.com>
2024-08-03 12:52:55 +0300

    tests

--- Server Files/APoptimal-server/functions/index.js
+++ Server Files/APoptimal-server/functions/index.js
@@ -3,18 +3,20 @@ const { spawn } = require('child_process');
 const path = require('path');
 
 exports.app = functions.https.onRequest((req, res) => {
-    console.log("in index.js")
-  const app = spawn('python', [path.resolve(__dirname, '../APoptimal-server/app.py')]);
+    console.log("in index.js - request received");
+    const app = spawn('python', [path.resolve(__dirname, '../APoptimal-server/app.py')]);
 
-  app.stdout.on('data', (data) => {
-    res.write(data);
-  });
+    app.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        res.write(data);
+    });
 
-  app.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    app.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
 
-  app.on('close', (code) => {
-    res.end();
-  });
+    app.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        res.end();
+    });
 });
