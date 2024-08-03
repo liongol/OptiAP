7839cae7a96603436a893888daab6ed4383e4efb
Yanai99 <yanaiparan@gmail.com>
2024-08-03 12:44:42 +0300

    tests

--- Server Files/APoptimal-server/functions/index.js
+++ Server Files/APoptimal-server/functions/index.js
@@ -2,18 +2,21 @@ const functions = require('firebase-functions');
 const { exec } = require('child_process');
 const path = require('path');
 
+// Define the function to handle HTTP requests and route them to the Flask app
 exports.app = functions.https.onRequest((req, res) => {
-  const app = exec(`python ${path.resolve(__dirname, '../app.py')}`);
-
-  app.stdout.on('data', (data) => {
+  // Adjust the path to your app.py file relative to the functions directory
+  console.log("before appProcess")
+  const appProcess = exec(`python ${path.resolve(__dirname, '../APoptimal-server/app.py')}`);
+  console.log("after appProcess")
+  appProcess.stdout.on('data', (data) => {
     res.write(data);
   });
 
-  app.stderr.on('data', (data) => {
+  appProcess.stderr.on('data', (data) => {
     console.error(`stderr: ${data}`);
   });
 
-  app.on('close', (code) => {
+  appProcess.on('close', (code) => {
     res.end();
   });
 });
