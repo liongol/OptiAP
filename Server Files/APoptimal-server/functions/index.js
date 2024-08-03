1e765c545b167a8762660e9ffc6764f1a021525c
Yanai99 <yanaiparan@gmail.com>
2024-08-03 13:08:20 +0300

    tests

--- Server Files/APoptimal-server/functions/index.js
+++ Server Files/APoptimal-server/functions/index.js
@@ -1,10 +1,20 @@
 const functions = require('firebase-functions');
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 const path = require('path');
+
+// Run the setup.sh script to install Python dependencies
+exec('bash functions/setup.sh', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error running setup.sh: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+});
 
 exports.app = functions.https.onRequest((req, res) => {
     console.log("in index.js - request received");
-    const app = spawn('python', [path.resolve(__dirname, '../APoptimal-server/app.py')]);
+    const app = exec(`python ${path.resolve(__dirname, '../APoptimal-server/app.py')}`);
 
     app.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
